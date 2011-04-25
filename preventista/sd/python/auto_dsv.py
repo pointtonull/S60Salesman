eefe8ef8b1ff4d9ebfa8230aec4017c63507954e
Carlos Cabrera <point_to_null@esdebian.org>
2011-04-24 21:32:20 -0300

    fixing libs, try1

---
+++ preventista/sd/python/auto_dsv.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+import DSV
+
+def import_dsv(filename):
+    data = open(filename).read()
+    data = data.replace("\r\n", "\n")
+    qualifier = DSV.guessTextQualifier(data)
+    data = DSV.organizeIntoLines(data, textQualifier=qualifier)
+    delimiter = DSV.guessDelimiter(data)
+    data = DSV.importDSV(data, delimiter=delimiter, textQualifier=qualifier)
+
+    hasheader = DSV.guessHeaders(data)
+
+    columns = len(data[0])
+    if hasheader:
+        header = data.pop(0)
+    else:
+        header = range(1, columns + 1)
+
+    dsv = []
+    for row in data:
+        mapped_row = {}
+        for column in xrange(columns):
+            mapped_row[header[column]] = row[column]
+        dsv.append(mapped_row)
+
+    return dsv
+
+
+def main():
+    print(import_dsv("data/input/clientes.csv"))
+
+if __name__ == "__main__":
+    exit(main())
